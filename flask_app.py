308f2fade6a22744255618067c9bde41550067c3
Seth J. Connell <sethjordanconnell@gmail.com>
2020-04-07 18:12:36 -0500

    Made Question Form A Post

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
+@app.route("/", methods=["GET", "POST"])
+def question():
+    if request.method == 'POST':
+            question=request.form['question']
+            return "Wow"
+    
     return render_template('index.html')
 
 if __name__ == '__main__':
