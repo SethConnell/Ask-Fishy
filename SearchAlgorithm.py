5eb269f31a743e4bd47dca62954c37e080bd2d70
Seth J. Connell <sethjordanconnell@gmail.com>
2020-04-22 13:49:40 -0500

    Fixed Search Algorithm Bug

--- SearchAlgorithm.py
+++ SearchAlgorithm.py
@@ -1,15 +1,16 @@
 import re
 from DBFunctions import *
+# Right now, this search function is vulnerable to SQL injection. It will be fixed.
 def search(question):
     fixedquery = re.sub('\W', '', question)
-
-    sql = '''SELECT * FROM Threads WHERE `question` = "''' + str(fixedquery) + '"'
+    sql = "SELECT * FROM Threads WHERE `question` = '%s'" % fixedquery
     records = db.query(sql).fetchone()
 
     if records is not None:
         threadid = records[1]
         return int(threadid)
     else:
-        sql = "INSERT INTO `Threads` (question) VALUES ('" + str(fixedquery) + "')"
+        sql = "INSERT INTO `Threads` (question) VALUES ('%s')" % fixedquery
+
         threadid = db.query(sql).lastrowid
         return int(threadid)
